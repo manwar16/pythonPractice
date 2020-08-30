992f6f48190bbf478660b959d00df1791dbaab32
manwar16 <mdmanwarcse16@gmail .com>
2020-08-30 23:37:19 +0600

    function Program

---
+++ function.py
@@ -0,0 +1,14 @@
+def add_tow_number(first, second):
+    return first+second
+
+sum=add_tow_number(3,4)
+print(sum)
+
+def printSomthing(num1, num2):
+    print(f"You passed: {num1} {num2}")
+
+printSomthing(4,8)
+
+number1=34
+number2=12
+printSomthing(number1, number2)
